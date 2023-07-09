ddc18d1594d87607dd61d59897f5566c2386035c
Thomas Peruch <peruchthomas@gmail.com>
2023-07-09 17:22:25 -0300

    add spring doc dependecies, integration with swagger ui, rename SessionController to SessionStatusController

--- src/main/java/com/tperuch/voteservice/endpoint/SessionStatusController.java
+++ src/main/java/com/tperuch/voteservice/endpoint/SessionStatusController.java
@@ -2,6 +2,8 @@ package com.tperuch.voteservice.endpoint;
 
 import com.tperuch.voteservice.dto.response.SessionStatusResponseDto;
 import com.tperuch.voteservice.service.SessionStatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/session-status")
-public class SessionController {
+@Tag(name = "Status da sessão", description = "Operações relacionadas a status de sessão")
+public class SessionStatusController {
     @Autowired
     public SessionStatusService statusService;
 
+    @Operation(summary = "Busca os status das sessões")
     @GetMapping
     public ResponseEntity<List<SessionStatusResponseDto>> getAllSessionsStatus() {
         return new ResponseEntity<>(statusService.getAllSessions(), HttpStatus.OK);
--- src/main/java/com/tperuch/voteservice/endpoint/VoteController.java
+++ src/main/java/com/tperuch/voteservice/endpoint/VoteController.java
@@ -3,6 +3,8 @@ package com.tperuch.voteservice.endpoint;
 import com.tperuch.voteservice.dto.VoteDto;
 import com.tperuch.voteservice.dto.response.VoteResultResponseDto;
 import com.tperuch.voteservice.service.VoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +14,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/vote")
+@Tag(name = "Votos", description = "Operações relacionadas a votos")
 public class VoteController {
     @Autowired
     public VoteService voteService;
 
+    @Operation(summary = "Registra voto em uma sessão")
     @PostMapping
     public ResponseEntity<VoteDto> vote(@Valid @RequestBody VoteDto voteDto) {
         return new ResponseEntity<>(voteService.saveVote(voteDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Conta votos de uma sessão e mostra o resultado")
     @GetMapping(value = "/{sessionId}")
     public ResponseEntity<VoteResultResponseDto> getVotesAndSessionResult(@PathVariable @NotNull Long sessionId) {
         return new ResponseEntity<>(voteService.getVotesAndSessionResult(sessionId), HttpStatus.OK);
